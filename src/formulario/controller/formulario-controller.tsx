abad95be4ad734ca905ab734e8afa52cffa9bf75
ZugarP <jpcx1999@gmail.com>
2023-03-05 20:10:07 -0300

    05-03-2023

---
+++ src/formulario/controller/formulario-controller.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormularioView from "../view/formulario-view";
+
+export default function FormularioController(){
+    return(
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
